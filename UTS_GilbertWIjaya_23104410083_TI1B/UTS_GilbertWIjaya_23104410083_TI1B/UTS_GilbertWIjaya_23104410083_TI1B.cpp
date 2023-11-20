1ceb4232e6a6e99c64f3e46a5af80e9a6d8b53a4
ACER ASPIRE <yhsgilbertyhs@gmail.com>
2023-11-20 14:56:31 +0700

    update gilbertwijaya

--- UTS_GilbertWIjaya_23104410083_TI1B/UTS_GilbertWIjaya_23104410083_TI1B/UTS_GilbertWIjaya_23104410083_TI1B.cpp
+++ UTS_GilbertWIjaya_23104410083_TI1B/UTS_GilbertWIjaya_23104410083_TI1B/UTS_GilbertWIjaya_23104410083_TI1B.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 int main() {
-    // Deklarasi variabel
+    
     string nama;
-    int golongan, jam_kerja, upah, lembur, gaji;
-    // Masukkan nama karyawan
-    cout << "Masukkan nama karyawan: ";
-    cin >> nama;
-    // Masukkan golongan kerja
-    cout << "Masukkan golongan kerja (1, 2, atau 3): ";
+    int gaji, lemburan, jamKerja, golongan,totalGj;
+
+    cout << "masukan nama anda: ";
+    getline(cin, nama);
+    cout << "masukan golongan anda: ";
     cin >> golongan;
-    // Masukkan jam kerja
-    cout << "Masukkan jam kerja per minggu: ";
-    cin >> jam_kerja;
-    // Validasi input
-    if (golongan < 1 || golongan > 3) {
-        cout << "Golongan kerja tidak valid" << endl;
-        return 0;
-    }
-    if (jam_kerja < 0) {
-        cout << "Jam kerja tidak valid" << endl;
-        return 0;
-    }
-    // Hitung upah per jam berdasarkan golongan
-    switch (golongan) {
+    cout << "masukan jam kerja anda per minggu: ";
+    cin >> jamKerja;
+
+
+    switch (golongan)
+    {
     case 1:
-        upah = 25000;
+        gaji = 25000;
         break;
     case 2:
-        upah = 35000;
+        gaji = 35000;
         break;
     case 3:
-        upah = 50000;
+        gaji = 50000;
+        break;
+    default:
+        cout << "golongan tidak valid" << endl;
         break;
     }
-    // Hitung uang lembur jika jam kerja lebih dari 48 jam
-    if (jam_kerja > 48) {
-        lembur = (jam_kerja - 48) * 10000;
+    
+    lemburan = (jamKerja - 48) * 10000;
+    if (jamKerja < 48)
+    {
+        totalGj = gaji * jamKerja;
+        cout << "total gaji " << nama << " adalah: " << totalGj << endl;
+    }
+    else if (jamKerja > 48) {
+
+        totalGj = (gaji * jamKerja) + lemburan;
+        cout << "total gaji " << nama << " adalah: " << totalGj << endl;
+
     }
     else {
-        lembur = 0;
+        cout << "konteks tidak valid" << endl;
     }
-    // Hitung gaji per minggu
-    gaji = (upah * jam_kerja) + lembur;
-    // Cetak gaji karyawan
-    cout << "Nama karyawan: " << nama << endl;
-    cout << "Golongan kerja: " << golongan << endl;
-    cout << "Jam kerja per minggu: " << jam_kerja << endl;
-    cout << "Gaji per minggu: Rp " << gaji << endl;
+
+
+    
     return 0;
 }
